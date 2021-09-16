f1a97a13a8322487bb4220b7c8049dc82a6fbf57
ameurbesbes <ameurbesbes@outlook.com>
2021-09-17 00:21:05 +0100

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,23 +7,23 @@ import sys
 
 
 if __name__ == '__main__':
-	id_user = sys.argv[1]
-	name = ""
-	total = 0
-	done = 0
-	arr = []
-	data = requests.get(
-		"https://jsonplaceholder.typicode.com/users/{}".format(
-			int(id_user))).json()
-	name = data["name"]
-	url = "https://jsonplaceholder.typicode.com/todos"
-	response = requests.get(url)
-	for elm in response.json():
-		if elm['userId'] == int(id_user):
-			total += 1
-			if elm['completed'] is True:
-				done += 1
-				arr.append(elm['title'])
-	print("Employee {} is done with tasks({}/{}):".format(name, done, total))
-	for x in arr:
-		print("\t{}".format(x))
+    id_user = sys.argv[1]
+    name = ""
+    total = 0
+    done = 0
+    arr = []
+    data = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(
+            int(id_user))).json()
+    name = data["name"]
+    url = "https://jsonplaceholder.typicode.com/todos"
+    response = requests.get(url)
+    for elm in response.json():
+        if elm['userId'] == int(id_user):
+            total += 1
+            if elm['completed'] is True:
+                done += 1
+                arr.append(elm['title'])
+    print("Employee {} is done with tasks({}/{}):".format(name, done, total))
+    for x in arr:
+        print("\t{}".format(x))
