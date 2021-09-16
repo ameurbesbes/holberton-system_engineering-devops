a681614ef237e0ffc2dd818d408e032b5852dc81
ameurbesbes <ameurbesbes@outlook.com>
2021-09-17 00:16:09 +0100

    update

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+gather data model
+"""
+import requests
+import sys
+
+id_user = sys.argv[1]
+name = ""
+total = 0
+done = 0
+arr = []
+data = requests.get(
+    "https://jsonplaceholder.typicode.com/users/{}".format(
+        int(id_user))).json()
+name = data["name"]
+url = "https://jsonplaceholder.typicode.com/todos"
+response = requests.get(url)
+for elm in response.json():
+    if elm['userId'] == int(id_user):
+        total += 1
+        if elm['completed'] is True:
+            done += 1
+            arr.append(elm['title'])
+print("Employee {} is done with tasks({}/{}):".format(name, done, total))
+for x in arr:
+    print("\t{}".format(x))
