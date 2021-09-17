73d47bcec91859012919e9a3ad305a3ccd55a971
ameurbesbes <ameurbesbes@outlook.com>
2021-09-17 02:07:07 +0100

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -2,9 +2,8 @@
 """
 gather data model
 """
-import csv
-import requests
 import json
+import requests
 import sys
 
 
@@ -30,14 +29,6 @@ if __name__ == '__main__':
     eq = requests.get(
         'https://jsonplaceholder.typicode.com/users/' +
         (id_user) + '/todos')
-    data = {}
-    data[id_user] = []
-    task = {}
-    for x in eq.json():
-        task['task'] = x["title"]
-        task["completed"] = x["completed"]
-        task["username"] = username
-        data[id_user].append(task)
-    json_object = json.dumps(data, indent=4)
     with open("{}.json".format(id_user), "w") as outfile:
-        outfile.write(json_object)
+        json.dump({id_user: [{"title": x.get("title"), "completed": x.get(
+            "completed"), "username": username}for x in eq.json()]}, outfile)
